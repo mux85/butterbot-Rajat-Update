f88981c0e305f6a5a69f385353134e7d9a369c26
rajathooda <127722895+rajathooda@users.noreply.github.com>
2023-08-07 17:57:48 +0530

    ignorets

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ const nextConfig = {
     // This allows production builds to complete even if there are ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig;
